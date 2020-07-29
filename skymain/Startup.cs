e65dbc8d8755f1a9fad1ec2ce0dbd63bc4667441
Park Minu <wminos@gmail.com>
2020-07-30 01:04:25 +0900

    use api endpoint using proxy pass

--- skymain/Startup.cs
+++ skymain/Startup.cs
@@ -25,6 +25,7 @@ namespace skymain
         {
             services.AddRazorPages();
             services.AddLogging(logging => logging.AddAWSProvider());
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
